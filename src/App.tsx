f62e1add43376da53b6924d42bb61c351873a939
IsaqueTADS <isaqueofc18@gmail.com>
2025-10-17 23:36:22 -0300

    rotas principais

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,26 @@ import Header from "./components/Header";
 import { DataContextProvider } from "./context/DataContext";
 import Resumo from "./pages/Resumo";
 import Vendas from "./pages/Vendas";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Venda from "./pages/Venda";
 
 const App = () => {
   return (
-    <DataContextProvider>
-      <div className="container">
-        <Sidenav />
-        <main>
-          <Header />
-          <Resumo />
-          <Vendas />
-        </main>
-      </div>
-    </DataContextProvider>
+    <BrowserRouter>
+      <DataContextProvider>
+        <div className="container">
+          <Sidenav />
+          <main>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Resumo />} />
+              <Route path="/vendas" element={<Vendas />} />
+              <Route path="/vendas/:id" element={<Venda />} />
+            </Routes>
+          </main>
+        </div>
+      </DataContextProvider>
+    </BrowserRouter>
   );
 };
 
